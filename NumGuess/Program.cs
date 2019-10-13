0da77ae8695c3cfe522385b95611aeef5c4cc75f
noony9 <noony9@users.noreply.github.com>
2019-10-13 19:25:23 +0300

    progress2

--- NumGuess/Program.cs
+++ NumGuess/Program.cs
@@ -24,7 +24,7 @@ namespace NumGuess
             Console.WriteLine("{0}, Version {1}, written by {2}", appName, appVersion, appAuthor);
 
             //Display app purpose
-            Console.WriteLine("Num Guess is an extremely popular and hardcore game where the system prompts you to guess a number between 1 and 10!")
+            Console.WriteLine("Num Guess is an extremely popular and hardcore game where the system prompts you to guess a number between 1 and 10!");
 
             //Ask users name
             Console.WriteLine("What is your name?");
@@ -45,25 +45,42 @@ namespace NumGuess
             Console.WriteLine("Guess a number between 1 and 10");
 
             //While guess is not correct
-            while(userGuess != sysNumber)
+            while (userGuess != sysNumber)
             {
                 //Get user's input
                 string input = Console.ReadLine();
 
+                //Ensure user enters a number
+                if (!int.TryParse(input, out userGuess))
+                {
+                    //Change text color
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+                    //Inform user of error
+                    Console.WriteLine("Error, not a number, please enter a valid number");
+
+                    //Continue program
+                    continue;
+                }
                 //Cast input to an integer and assign to userGuess
                 userGuess = Int32.Parse(input);
 
                 //Match the system number to the user's guess
-                if(userGuess != sysNumber)
+                if (userGuess != sysNumber)
                 {
                     //Change text color
                     Console.ForegroundColor = ConsoleColor.DarkRed;
 
-                    //Inform user of error and prompt for another entry
+                    //Inform user of error
                     Console.WriteLine("Error, wrong number, please enter another value");
+                }
+                else if (userGuess == sysNumber)
+                {
+                    //Change text color
+                    Console.ForegroundColor = ConsoleColor.Green;
 
-                    
-
+                    //Inform user of correct answer
+                    Console.WriteLine("That is correct!!  I can't believe it!");
                 }
 
             }
