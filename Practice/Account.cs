414b5b90ab13030c80bd165fdce301bda8866613
noony9 <noony9@users.noreply.github.com>
2019-10-19 22:20:12 +0300

    classes

---
+++ Practice/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Account
+    {
+        private string name; // instance variable -- default value = null
+
+        // method that sets the account name in the object
+        public void SetName(string accountName)
+        {
+            name = accountName; // store the account name
+        }
+
+        // method that retrieves the account name from the object
+        public string GetName()
+        {
+            return name; // returns name's value to this method's caller
+        }
+    }
+}
---
+++ Practice/AccountTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class AccountTest
+    {
+        static void Main()
+        {
+            // create an account object and assign it to myAccount
+            Account myAcount = new Account();
+
+            // display myAccount's initial name (there isn't one yet)
+            Console.WriteLine($"Initial name is: {myAccount.GetName()}");
+
+            // prompt for and read the name, then put the name in the objet
+            Console.Write("Enter the name: "); // prompt
+            string theName = Console.ReadLine(); // read the name
+            myAccount.SetName(theName); // put theName in the myAccount object
+
+            // display the name stored in the myAccount object
+            Console.WriteLine($"myAccount's name is: {myAccount.GetName()}");
+        }
+    }     
+                     
+}
